383234d8d741c33bbfd03a3ff6e56b45ce3393ea
Paulino Joel Martinez Vasquez <joel98vasquez@gmail.com>
2022-08-16 17:02:20 -0500

    ejemplo, biosarti

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-miprimervariable = "hola"
-print(miprimervariable)
-num = 2
-print(num+2)
+print('ingrese codigo')
+
+print('CALCULO POR:')
+print('1.piezas')
+print('2.Ml Parcial')
+print('3.Ml Entero')
+print('4.Dia')
+seleccion = input()
+print(seleccion)
+
+match seleccion:
+    case '1':
+        print("el calculo por piezas es")
+        a=250/3
+        print(a)
+    case '2':
+        print("el calculo por Ml parcial es")
+        a = 250 / 3
+        print(a)
+    case '3':
+        print("el calculo por Ml Entero es")
+        a = 250 / 3
+        print(a)
+    case '4':
+        print("el calculo por Dia es")
+        a = 250 / 3
+        print(a)
+    case _:
+        print('operacion no encotrado')
+
